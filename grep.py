b26602e0c8ae0a10a986adc1b5a2bcdc8b051d57
tomas-rosak <93378161+tomas-rosak@users.noreply.github.com>
2022-04-23 12:41:01 +0200

    Update grep.py

--- grep.py
+++ grep.py
@@ -2,17 +2,21 @@
 import sys
 
 prikaz = sys.argv[1:]
-try:       
+if len(prikaz) < 2:
+    print("Malo argumentu")
+    exit(1)
+try:
     with open(prikaz[1]) as text:
         text = text.read()
-    text = text.split("\n")
-    pocet = 0
-    for i in text:
-        if prikaz[0] in i:
-            if len(prikaz) == 2:
-                print(i)
-            pocet += 1 
-    if len(prikaz) == 3 and prikaz[-1] == "-c":
-        print("Slovo", prikaz[0], "se v textu nachazi", pocet)
 except:
-    print("Chyba")
+    print("Nespravna cesta")
+    exit(1)
+text = text.split("\n")
+pocet = 0
+for i in text:
+    if prikaz[0] in i:
+        if len(prikaz) == 2:
+            print(i)
+        pocet += 1 
+if len(prikaz) == 3 and prikaz[-1] == "-c":
+    print(pocet)
